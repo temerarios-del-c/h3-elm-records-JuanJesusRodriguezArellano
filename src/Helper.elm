2123884f35c0fa4bdf560e1ed2f4a3a963f3246f
JJRA <up230253@alumnos.upa.edu.mx>
2026-02-15 14:29:07 -0600

    Test 7.0

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,6 +1,10 @@
 module Helper exposing (..)
 
--- 1. Ejercicios Simples
+import Html exposing (..)
+
+
+
+--Type annotation
 
 
 add2 : Int -> Int -> Int
@@ -9,8 +13,8 @@ add2 int1 int2 =
 
 
 add3 : Float -> Float -> Float -> Float
-add3 f1 f2 f3 =
-    f1 + f2 + f3
+add3 val1 val2 val3 =
+    val1 + val2 + val3
 
 
 calc : Int -> Int -> (Int -> Int -> Int) -> Int
@@ -19,51 +23,38 @@ calc int1 int2 operator =
 
 
 
--- 2. Records (Lenguajes)
+--Programas o lenguages
 
 
-type alias Language =
-    { name : String
-    , releaseYear : Int
-    , currentVersion : String
-    }
-
-
-languages : List Language
-languages =
-    [ { name = "Elm", releaseYear = 2012, currentVersion = "0.19.1" }
-    , { name = "Javascript", releaseYear = 1995, currentVersion = "ES6" }
+langs : List { name : String, releaseYear : Int, currentVersion : String }
+langs =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
     ]
 
 
-languageNames : List Language -> List String
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
 languageNames list =
     List.map .name list
 
 
 
--- 3. Records (Usuarios)
+-- Estudiante
 
 
-type alias User =
-    { name : String
-    , uType : String
-    }
-
-
-users : List User
+users : List { name : String, uType : String }
 users =
     [ { name = "Roberto", uType = "Student" }
     , { name = "Mitsiu", uType = "Professor" }
     ]
 
 
-onlyStudents : List User -> List String
+onlyStudents : List { name : String, uType : String } -> List String
 onlyStudents list =
     List.map
-        (\u ->
-            if u.uType == "Student" then
-                u.name
+        (\user ->
+            if user.uType == "Student" then
+                user.name
 
             else
                 ""
@@ -71,33 +62,30 @@ onlyStudents list =
         list
 
 
-
--- 4. Aliases (Videojuegos)
-
-
 type alias Videogame =
-    { title : String
-    , releaseYear : Int
-    , available : Bool
-    , downloads : Int
-    , genres : List String
-    }
+    { title : String, releaseYear : Int, available : Bool, downloads : Int, genres : List String }
 
 
 games : List Videogame
 games =
-    [ { title = "Control", releaseYear = 2019, available = True, downloads = 100, genres = [ "Action" ] }
-    , { title = "Zelda", releaseYear = 1998, available = True, downloads = 200, genres = [ "Adventure" ] }
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 1
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of time"
+      , releaseYear = 1998
+      , available = True
+      , downloads = 2
+      , genres = [ "Action", "Adventure" ]
+      }
     ]
 
 
 getVideogameGenres : List Videogame -> List (List String)
 getVideogameGenres list =
     List.map .genres list
-
-
-
--- 5. Datos para el ejercicio HTML (Computer)
 
 
 type alias Computer =
@@ -111,7 +99,22 @@ type alias Computer =
 myLaptop : Computer
 myLaptop =
     { ram = "16GB"
-    , model = "Katana"
+    , model = "Katana GF66 11UE-245XES"
     , brand = "MSI"
-    , screenSize = "17''"
+    , screenSize = "15.6"
     }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
